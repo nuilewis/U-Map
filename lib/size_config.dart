e3b085f90021e3b7a5e7d4524332423f49cb8b1f
nuilewis <lewisnuikweh@gmail.com>
2021-09-03 13:58:02 +0100

    Added light and dark themes, size config for responsiveness, and constants

---
+++ lib/size_config.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static MediaQueryData _mediaQueryData;
+  static double screenWidth;
+  static double screenHeight;
+  static double defaultSize;
+  static Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+  }
+}
+
+double getRelativeScreenHeight(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight;
+
+  //Takes in your screen height and rationalises it to the height
+  // of an iphone 11.
+  // because the design was designed for an iphone 11 ie 896.0
+  return (inputHeight / 896.0) * screenHeight;
+}
+
+double getRelativeScreenWidth(double inputWidth) {
+  double screenWidth = SizeConfig.screenWidth;
+
+  //Takes in your screen height and rationalises it to the height
+  // of an iphone 11.
+  // because the design was designed for an iphone 11 ie 896.0
+  return (inputWidth / 896.0) * screenWidth;
+}
---
+++ lib/theme.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+///Themes
+
+///Light Theme
+
+ThemeData lightThemeData(BuildContext context) {
+  return ThemeData(
+      appBarTheme: appBarTheme,
+      primaryColor: cPrimaryColor,
+      accentColor: cPrimaryColorLight,
+      scaffoldBackgroundColor: Colors.white,
+      colorScheme: ColorScheme.light(),
+      backgroundColor: Colors.white,
+      iconTheme: IconThemeData(color: cBlackIconColor),
+      primaryIconTheme: IconThemeData(color: cBlackIconColor),
+      fontFamily: 'Poppins',
+      textTheme: TextTheme(
+        bodyText1: TextStyle(color: cTextColorBlack),
+        bodyText2: TextStyle(color: cTextColorBlack),
+        headline1: TextStyle(
+            color: cTextColorBlack, fontSize: 80, fontWeight: FontWeight.bold),
+        headline4: TextStyle(
+            color: cTextColorBlack, fontSize: 32, fontWeight: FontWeight.bold),
+      ));
+}
+
+///Dark Theme
+
+ThemeData darkThemeData(BuildContext context) {
+  return ThemeData(
+      appBarTheme: appBarTheme,
+      primaryColor: cPrimaryColor,
+      accentColor: cPrimaryColorLight,
+      scaffoldBackgroundColor: Colors.black,
+      colorScheme: ColorScheme.dark().copyWith(),
+      backgroundColor: Colors.white,
+      iconTheme: IconThemeData(color: cBlackIconColor),
+      primaryIconTheme: IconThemeData(color: cLightIconColor),
+      fontFamily: 'Poppins',
+      textTheme: TextTheme(
+        bodyText1: TextStyle(color: cTextColorWhite),
+        bodyText2: TextStyle(color: cTextColorWhite),
+        headline1: TextStyle(
+            color: cTextColorWhite, fontSize: 80, fontWeight: FontWeight.bold),
+        headline4: TextStyle(
+            color: cTextColorWhite, fontSize: 32, fontWeight: FontWeight.bold),
+      ));
+}
+
+AppBarTheme appBarTheme = AppBarTheme(color: Colors.transparent, elevation: 0);
