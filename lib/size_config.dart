ed597ed615fd3c6ae936c8d3e47384cd291d7dad
nuilewis <lewisnuikweh@gmail.com>
2021-09-08 17:03:58 +0100

    Made size_config.dart null safe

--- lib/size_config.dart
+++ lib/size_config.dart
@@ -1,34 +1,35 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SizeConfig {
-  static MediaQueryData _mediaQueryData;
-  static double screenWidth;
-  static double screenHeight;
-  static double defaultSize;
-  static Orientation orientation;
+  MediaQueryData? _mediaQueryData;
+  late double screenWidth;
+  late double screenHeight;
+  late Orientation orientation;
 
-  void init(BuildContext context) {
-    _mediaQueryData = MediaQuery.of(context);
+  init(BuildContext context) {
+    MediaQueryData _mediaQueryData = MediaQuery.of(context);
     screenWidth = _mediaQueryData.size.width;
     screenHeight = _mediaQueryData.size.height;
     orientation = _mediaQueryData.orientation;
   }
 }
 
-double getRelativeScreenHeight(double inputHeight) {
-  double screenHeight = SizeConfig.screenHeight;
-
+double getRelativeScreenHeight(BuildContext context, double inputHeight) {
+  MediaQueryData _mediaQueryData = MediaQuery.of(context);
+  double screenHeight = _mediaQueryData.size.height;
   //Takes in your screen height and rationalises it to the height
   // of an iphone 11.
   // because the design was designed for an iphone 11 ie 896.0
   return (inputHeight / 812.0) * screenHeight;
 }
 
-double getRelativeScreenWidth(double inputWidth) {
-  double screenWidth = SizeConfig.screenWidth;
+double getRelativeScreenWidth(BuildContext context, double inputWidth) {
+  MediaQueryData _mediaQueryData = MediaQuery.of(context);
+  double screenWidth = _mediaQueryData.size.width;
 
-  //Takes in your screen height and rationalises it to the height
+  //Takes in your screen height and rationalises it to the width
   // of an iphone 11.
-  // because the design was designed for an iphone 11 ie 896.0
+  // because the design was designed for an iphone 11 ie 375
   return (inputWidth / 375.0) * screenWidth;
 }
