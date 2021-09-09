346a3d9878fde09983a49e46c791572c21a75f3d
nuilewis <lewisnuikweh@gmail.com>
2021-09-09 19:05:32 +0100

    Adjusted stuff

--- lib/screens/homescreen/components/popular_places_list_item.dart
+++ lib/screens/homescreen/components/popular_places_list_item.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:u_map/size_config.dart';
@@ -5,34 +6,45 @@ import 'package:u_map/size_config.dart';
 class PopularPlacesListItem extends StatelessWidget {
   final String title;
   final String? imageSrc;
+  final GeoPoint markerGeopoint;
+  final VoidCallback? onPressed;
 
-  const PopularPlacesListItem({Key? key, required this.title, this.imageSrc})
+  const PopularPlacesListItem(
+      {Key? key,
+      required this.title,
+      required this.markerGeopoint,
+      this.imageSrc,
+      this.onPressed})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
     //Popular place list item
-    return Container(
-      margin:
-          EdgeInsets.only(left: getRelativeScreenWidth(context, 20), right: 0),
-      width: getRelativeScreenWidth(context, 150),
-      height: getRelativeScreenHeight(context, 240),
-      decoration: BoxDecoration(
-        color: Theme.of(context).primaryColor,
-        borderRadius: BorderRadius.circular(
-          getRelativeScreenWidth(context, 30),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        margin: EdgeInsets.only(
+            left: getRelativeScreenWidth(context, 20), right: 0),
+        width: getRelativeScreenWidth(context, 150),
+        height: getRelativeScreenHeight(context, 240),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(
+            getRelativeScreenWidth(context, 30),
+          ),
+          //   image: DecorationImage(
+          //     image: NetworkImage(imagesrc),
+          //   ),
         ),
-        //   image: DecorationImage(
-        //     image: NetworkImage(imagesrc),
-        //   ),
-      ),
-      child: Stack(
-        children: [
-          Positioned(
-            bottom: getRelativeScreenHeight(context, 15),
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
+        child: Padding(
+          padding:
+              EdgeInsets.only(bottom: getRelativeScreenHeight(context, 20)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8),
                   child: Text(
                     title,
                     style: Theme.of(context).textTheme.bodyText1,
@@ -40,7 +52,10 @@ class PopularPlacesListItem extends StatelessWidget {
                     overflow: TextOverflow.clip,
                   ),
                 ),
-                IconButton(
+              ),
+              Expanded(
+                flex: 2,
+                child: IconButton(
                   padding: EdgeInsets.all(0),
                   visualDensity: VisualDensity.compact,
                   iconSize: 12,
@@ -51,11 +66,11 @@ class PopularPlacesListItem extends StatelessWidget {
                     color: Theme.of(context).iconTheme.color,
                   ),
                   onPressed: () {},
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
