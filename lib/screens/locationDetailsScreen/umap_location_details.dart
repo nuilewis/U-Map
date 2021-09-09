7226e484bb6a9e757628c395d24ef8c497608473
nuilewis <lewisnuikweh@gmail.com>
2021-09-09 19:04:26 +0100

    adjusted Location page UI, begun replacing dummy data with live data

--- lib/screens/locationDetailsScreen/umap_location_details.dart
+++ lib/screens/locationDetailsScreen/umap_location_details.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:u_map/screens/homescreen/components/umap_icon_button.dart';
-import 'package:u_map/screens/homescreen/components/umap_top_search_area.dart';
 import 'package:u_map/size_config.dart';
 
 import 'components/location_details_description.dart';
 
 class UmapLocationDetails extends StatefulWidget {
+  final String name;
+  final String description;
+  final String? imgSrc;
+  final LatLng markerLocation;
+
+  const UmapLocationDetails(
+      {Key? key,
+      required this.name,
+      required this.description,
+      this.imgSrc,
+      required this.markerLocation})
+      : super(key: key);
   @override
   _UmapLocationDetailsState createState() => _UmapLocationDetailsState();
 }
@@ -17,187 +29,166 @@ class _UmapLocationDetailsState extends State<UmapLocationDetails> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      // appBar: AppBar(
-      //   leading: IconButton(
-      //     icon: SvgPicture.asset(
-      //       "assets/svg/back_icon.svg",
-      //       color: Theme.of(context).iconTheme.color,
-      //     ),
-      //     onPressed: () {
-      //       Navigator.pop(context);
-      //     },
-      //   ),
-      //   actions: [
-      //     IconButton(
-      //       icon: SvgPicture.asset(
-      //         "assets/svg/menu_icon.svg",
-      //         color: Theme.of(context).iconTheme.color,
-      //       ),
-      //       onPressed: () {
-      //         Navigator.pop(context);
-      //       },
-      //     ),
-      //   ],
-      // ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ///Top bar and menu
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: getRelativeScreenWidth(context, 20),
-                  vertical: getRelativeScreenHeight(context, 30),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      icon: SvgPicture.asset(
-                        "assets/svg/back_icon.svg",
-                        color: Theme.of(context).iconTheme.color,
-                      ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                    ),
-                    IconButton(
-                      icon: SvgPicture.asset(
-                        "assets/svg/menu_icon.svg",
-                        color: Theme.of(context).iconTheme.color,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-              ),
-
-              ///Picture container
-              Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: getRelativeScreenWidth(context, 20),
-                ),
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height * .65,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                  borderRadius: BorderRadius.circular(
-                    getRelativeScreenWidth(context, 32),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: getRelativeScreenHeight(context, 20),
-              ),
-
-              ///Buttons Section Below
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: getRelativeScreenWidth(context, 20),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: getRelativeScreenWidth(context, 5),
-                    ),
-
-                    ///Distance Text
-                    RichText(
-                      text: TextSpan(
-                        children: [
-                          TextSpan(
-                              text: "1.3",
-                              style: Theme.of(context).textTheme.headline1),
-                          TextSpan(
-                              text: " km",
-                              style: Theme.of(context).textTheme.headline2),
-                        ],
-                      ),
-                    ),
-
-                    ///Add to  and remove from saved Button
-                    UmapIconButton(
-                      iconLink: "assets/svg/heart_icon.svg",
-                      onPressed: () {},
-                      bgColor: Theme.of(context).primaryColorLight,
-                    ),
-
-                    ///Get Directions Button
-                    Container(
-                      height: getRelativeScreenWidth(context, 60),
-                      width: getRelativeScreenWidth(context, 150),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(
-                          getRelativeScreenWidth(context, 20),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Get Directions",
-                            style: Theme.of(context).textTheme.bodyText2,
-                          ),
-                          SizedBox(
-                            width: getRelativeScreenWidth(context, 5),
-                          ),
-                          SvgPicture.asset(
-                            "assets/svg/direction_icon.svg",
-                            //height: 20,
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-
-              SizedBox(
-                height: getRelativeScreenHeight(context, 20),
-              ),
-
-              ///Description Section
-              // Padding(
-              //   padding: EdgeInsets.symmetric(
-              //       horizontal: getRelativeScreenWidth(context, 20)),
-              //   child: Container(
-              //     child: Column(
-              //       crossAxisAlignment: CrossAxisAlignment.start,
-              //       children: [
-              //         Text(
-              //           "The Central Adiministration",
-              //           style: Theme.of(context).textTheme.headline2,
-              //           textAlign: TextAlign.left,
-              //         ),
-              //         Divider(
-              //           color: Theme.of(context).primaryColorLight,
-              //           thickness: getRelativeScreenHeight(context, 5),
-              //           endIndent: MediaQuery.of(context).size.width * .7,
-              //         ),
-              //         SizedBox(
-              //           height: getRelativeScreenHeight(context, 15),
-              //         ),
-              //         Text(
-              //             "The Central Admin description, tka;lja;a;aja, avjakja ahusvh asuhfa fasfharhf ahefsjdha hadhfauhfa;lh ahsdfhapug aghapoghahg aoghahjhfaP AGHAORHGA AOGIHA GHAHA;Hhs;ho AHAHAOHA al;agh aihaoh"),
-              //         SizedBox(height: getRelativeScreenHeight(context, 30)),
-              //       ],
-              //     ),
-              //   ),
-              // ),
-              LocDetailsDescriptionSection(
-                name: "The Central Adiministration",
-                description: ""
-                    "The Central Admin description, tka;lja;a;aja, avjakja ahusvh asuhfa fasfharhf ahefsjdha hadhfauhfa;lh ahsdfhapug aghapoghahg"
-                    " aoghahjhfaP AGHAORHGA AOGIHA"
-                    " GHAHA;Hhs;ho AHA"
-                    "",
-              ),
-            ],
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        toolbarHeight: getRelativeScreenHeight(context, 90),
+        elevation: 0,
+        foregroundColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: SvgPicture.asset(
+            "assets/svg/back_icon.svg",
+            color: Theme.of(context).iconTheme.color,
           ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(
+              right: getRelativeScreenWidth(context, 15),
+            ),
+            child: IconButton(
+              icon: SvgPicture.asset(
+                "assets/svg/menu_icon.svg",
+                color: Theme.of(context).iconTheme.color,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: getRelativeScreenHeight(context, 120)),
+            // ///Top bar and menu
+            // Padding(
+            //   padding: EdgeInsets.symmetric(
+            //     horizontal: getRelativeScreenWidth(context, 20),
+            //     vertical: getRelativeScreenHeight(context, 30),
+            //   ),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //     children: [
+            //       IconButton(
+            //         icon: SvgPicture.asset(
+            //           "assets/svg/back_icon.svg",
+            //           color: Theme.of(context).iconTheme.color,
+            //         ),
+            //         onPressed: () {
+            //           Navigator.pop(context);
+            //         },
+            //       ),
+            //       IconButton(
+            //         icon: SvgPicture.asset(
+            //           "assets/svg/menu_icon.svg",
+            //           color: Theme.of(context).iconTheme.color,
+            //         ),
+            //         onPressed: () {},
+            //       ),
+            //     ],
+            //   ),
+            // ),
+
+            ///Picture container
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: getRelativeScreenWidth(context, 20),
+              ),
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * .65,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(
+                  getRelativeScreenWidth(context, 32),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: getRelativeScreenHeight(context, 20),
+            ),
+
+            ///Buttons Section Below
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: getRelativeScreenWidth(context, 20),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: getRelativeScreenWidth(context, 5),
+                  ),
+
+                  ///Distance Text
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: "1.3",
+                            style: Theme.of(context).textTheme.headline1),
+                        TextSpan(
+                            text: " km",
+                            style: Theme.of(context).textTheme.headline2),
+                      ],
+                    ),
+                  ),
+
+                  ///Add to  and remove from saved Button
+                  UmapIconButton(
+                    iconLink: "assets/svg/heart_icon.svg",
+                    onPressed: () {},
+                    bgColor: Theme.of(context).primaryColorLight,
+                  ),
+
+                  ///Get Directions Button
+                  Container(
+                    height: getRelativeScreenWidth(context, 60),
+                    width: getRelativeScreenWidth(context, 150),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(
+                        getRelativeScreenWidth(context, 20),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Get Directions",
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        SizedBox(
+                          width: getRelativeScreenWidth(context, 5),
+                        ),
+                        SvgPicture.asset(
+                          "assets/svg/direction_icon.svg",
+                          color: Theme.of(context).iconTheme.color,
+                          //height: 20,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: getRelativeScreenHeight(context, 20),
+            ),
+
+            ///Description Section
+            LocDetailsDescriptionSection(
+              name: widget.name,
+              description: widget.description,
+            ),
+          ],
         ),
       ),
     );
