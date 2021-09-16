a0983a597ce300f2f9d8d971ae590f303f4ec7ac
nuilewis <lewisnuikweh@gmail.com>
2021-09-16 15:09:25 +0100

    Added dynamic list of saved items from SP

--- lib/screens/savedscreen/umap_savedscreen.dart
+++ lib/screens/savedscreen/umap_savedscreen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:u_map/components/umap_shared_preferences/umap_shared_preferences.dart';
+import 'package:u_map/components/umap_shared_preferences/umap_sp_methods.dart';
 import 'package:u_map/screens/findscreen/components/umap_list_item.dart';
+import 'package:u_map/screens/errorscreen/firebase_error_screen.dart';
 import 'package:u_map/screens/homescreen/components/umap_top_search_area.dart';
+import 'package:u_map/screens/locationDetailsScreen/umap_location_details.dart';
 
 import '../../size_config.dart';
 
@@ -10,32 +15,89 @@ class UMapSavedScreen extends StatefulWidget {
 }
 
 class _UMapSavedScreenState extends State<UMapSavedScreen> {
+  //List<UmapSaved> umapList = <UmapSaved>[];
+
+  @override
+  initState() {
+    super.initState();
+    initUmapSharedPreferences();
+  }
+
+  Widget buildSavedPlacesList(
+    BuildContext context,
+    int index,
+    UmapSaved savedItem,
+  ) {
+    return UmapListItem(
+      title: umapSPList[index].savedName,
+      //markerGeopoint: document["location"],
+      description: umapSPList[index].savedDescription,
+      imgSrc: null,
+      firstIconSvgLink: "assets/svg/trash_icon.svg",
+      firstIconOnPressed: () {
+        ///remove from saved list
+        removeFromSavedList(
+            UmapSaved(
+              savedName: umapSPList[index].savedName,
+              savedDescription: umapSPList[index].savedDescription,
+              savedDistance: umapSPList[index].savedDistance,
+              savedLocationLatitude: umapSPList[index].savedLocationLatitude,
+              savedLocationLongitude: umapSPList[index].savedLocationLongitude,
+            ),
+            umapSPList[index].savedName);
+      },
+
+      secondIconSvgLink: "assets/svg/forward_icon.svg",
+      secondIconOnPressed: () {
+        LatLng markerLoc = LatLng(umapSPList[index].savedLocationLatitude,
+            umapSPList[index].savedLocationLongitude);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => UmapLocationDetails(
+              name: umapSPList[index].savedName,
+              description: umapSPList[index].savedDescription,
+              markerLocation: markerLoc,
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            UmapTopSearchMenu(),
-            SizedBox(
-              height: getRelativeScreenHeight(context, 30),
-            ),
-            UmapListItem(
-              title: "The Central Administration",
-              description: "description of the central admin",
-              imgSrc: null,
-              firstIconSvgLink: "assets/svg/heart_icon.svg",
-              firstIconOnPressed: () {},
-              secondIconSvgLink: "assets/svg/forward_icon.svg",
-              secondIconOnPressed: () {},
-            )
-
-            // SizedBox(
-            //   height: getRelativeScreenHeight(40),
-            // ),
-          ],
+    if (umapSPList.isEmpty) {
+      return UmapErrorScreen(
+        errorMessage: "Nothing to see here",
+        errorDetails: "You haven't saved anything yet",
+      );
+    } else {
+      return Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: [
+              UmapTopSearchMenu(),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: getRelativeScreenHeight(context, 140),
+                ),
+                child: ListView.builder(
+                    itemCount: umapSPList.length,
+                    itemExtent: getRelativeScreenHeight(context, 190),
+                    itemBuilder: (BuildContext context, int index) {
+                      return buildSavedPlacesList(
+                          context, index, umapSPList[index]);
+                    }),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+
+        // SizedBox(
+        //   height: getRelativeScreenHeight(40),
+        // ),
+      );
+    }
   }
 }
