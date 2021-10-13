b3b5e01218d576998a44564249bd32a2eb8e8a6e
nuilewis <lewisnuikweh@gmail.com>
2021-10-14 00:55:45 +0100

    Added the new firestore backend

--- lib/screens/findscreen/umap_findscreen.dart
+++ lib/screens/findscreen/umap_findscreen.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:u_map/components/umapDrawer.dart';
 import 'package:u_map/components/umap_shared_preferences/umap_shared_preferences.dart';
 import 'package:u_map/components/umap_shared_preferences/umap_sp_methods.dart';
 import 'package:u_map/screens/findscreen/components/umap_list_item.dart';
-import 'package:u_map/screens/homescreen/components/umap_top_search_area.dart';
+import 'package:u_map/screens/homescreen/components/umap_app_bar.dart';
 import 'package:u_map/screens/locationDetailsScreen/umap_location_details.dart';
 import 'package:u_map/size_config.dart';
 
@@ -14,8 +16,26 @@ class UMapFindScreen extends StatefulWidget {
 }
 
 class _UMapFindScreenState extends State<UMapFindScreen> {
-  final Stream<QuerySnapshot> umapFirestoreStream =
-      FirebaseFirestore.instance.collection('umap_uba').snapshots();
+  ///TODO: find a way to merge all streams to one
+  final Stream<QuerySnapshot> umapAdministrativeFirestoreStream =
+      FirebaseFirestore.instance
+          .collection("umap_bamenda")
+          .doc("umap_uba")
+          .collection("administrative blocks")
+          .snapshots();
+  final Stream<QuerySnapshot> umapClassesFirestoreStream = FirebaseFirestore
+      .instance
+      .collection("umap_bamenda")
+      .doc("umap_uba")
+      .collection("classes")
+      .snapshots();
+  final Stream<QuerySnapshot> umapOfficesFirestoreStream = FirebaseFirestore
+      .instance
+      .collection("umap_bamenda")
+      .doc("umap_uba")
+      .collection("offices")
+      .snapshots();
+  // late final Stream<QuerySnapshot>? umapGlobalFirestoreStream;
   late LatLng markerLoc;
   String? savedIconLink;
   late VoidCallback onSavedPressed;
@@ -40,62 +60,61 @@ class _UMapFindScreenState extends State<UMapFindScreen> {
     }
     return UmapListItem(
       title: document["name"],
-      //markerGeopoint: document["location"],
+      sourceLocation:
+          LatLng(document["location"].latitude, document["location"].longitude),
       description: document["description"],
-      imgSrc: null,
+      imgSrc: document["imageUrl"],
       firstIconSvgLink: isSaved
           ? "assets/svg/heart_icon_filled.svg"
           : "assets/svg/heart_icon.svg",
       firstIconOnPressed: isSaved
           ? () {
               Feedback.forTap(context);
+              HapticFeedback.lightImpact();
               GeoPoint sourceLocation = document["location"];
               setState(() {
                 removeFromSavedList(
-                    UmapSaved(
+                    savedItem: UmapSaved(
                       savedName: document["name"],
                       savedDescription: document["description"],
-                      // savedDistance: calcDistance!.toStringAsFixed(2),
-                      savedDistance: "1km",
+                      savedImgUrl: document["imageUrl"],
                       savedLocationLatitude: sourceLocation.latitude.toDouble(),
                       savedLocationLongitude:
                           sourceLocation.latitude.toDouble(),
                     ),
-                    document["name"]);
+                    locationName: document["name"]);
                 isSaved = false;
               });
             }
           : () {
               Feedback.forTap(context);
+              HapticFeedback.lightImpact();
               GeoPoint sourceLocation = document["location"];
               setState(() {
                 addToSavedList(
-                  UmapSaved(
+                  savedItem: UmapSaved(
                     savedName: document["name"],
                     savedDescription: document["description"],
-
-                    ///Todo: Replace Saved Distance with actual Saved distance
-                    // savedDistance: calcDistance!.toStringAsFixed(2),
-                    savedDistance: "1km",
+                    savedImgUrl: document["imageUrl"],
                     savedLocationLatitude: sourceLocation.latitude.toDouble(),
                     savedLocationLongitude: sourceLocation.longitude.toDouble(),
                   ),
                 );
+
                 isSaved = true;
               });
             },
-
       secondIconSvgLink: "assets/svg/forward_icon.svg",
       secondIconOnPressed: () {
-        markerLoc = LatLng(
-            document["location"].latitude, document["location"].longitude);
         Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => UmapLocationDetails(
               name: document["name"],
               description: document["description"],
-              markerLocation: markerLoc,
+              markerLocation: LatLng(document["location"].latitude,
+                  document["location"].longitude),
+              imgSrc: document["imageUrl"],
             ),
           ),
         );
@@ -106,97 +125,191 @@ class _UMapFindScreenState extends State<UMapFindScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
+      extendBodyBehindAppBar: true,
+      //backgroundColor: Colors.transparent,
+      appBar: UmapAppBar(),
+      endDrawer: UmapDrawer(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
           children: [
-            UmapTopSearchMenu(),
+            ///Administrative blocks Stream
             Padding(
-              padding: EdgeInsets.only(
-                top: getRelativeScreenHeight(context, 140),
+              padding: EdgeInsets.all(getRelativeScreenWidth(context, 20)),
+              child: Text(
+                "Administrative Blocks",
+                style: Theme.of(context).textTheme.headline2,
               ),
-              child: StreamBuilder(
-                stream: umapFirestoreStream,
-                builder: (
-                  BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot,
-                ) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.waiting:
+            ),
+            StreamBuilder(
+              stream: umapAdministrativeFirestoreStream,
+              builder: (
+                BuildContext context,
+                AsyncSnapshot<QuerySnapshot> snapshot,
+              ) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator.adaptive(
+                        backgroundColor: Colors.transparent,
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                          Theme.of(context).iconTheme.color!,
+                        ),
+                      ),
+                    );
+                  default:
+                    List<DocumentSnapshot> umapSourceDocuments =
+                        snapshot.data!.docs;
+                    if (umapSourceDocuments.length == 0) {
                       return Center(
-                        child: CircularProgressIndicator.adaptive(
-                          backgroundColor: Colors.transparent,
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                            Theme.of(context).iconTheme.color!,
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Text(
+                            "Oh, there doesn't seem to be anything under this category",
+                            textAlign: TextAlign.left,
+                            style: Theme.of(context).textTheme.bodyText2,
                           ),
                         ),
                       );
-                    default:
-                      List<DocumentSnapshot> umapSourceDocuments =
-                          snapshot.data!.docs;
+                    } else {
                       return ListView.builder(
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
                           itemExtent: getRelativeScreenHeight(context, 195),
                           itemCount: umapSourceDocuments.length,
                           itemBuilder: (context, index) {
-                            //Loop to check if a location has already been saved or not
-
-                            // for (int i = 0; i < umapSPList.length; i++) {
-                            //   if (umapSPList[i].savedName ==
-                            //       umapSourceDocuments[index]["name"]) {
-                            //     savedIconLink =
-                            //         "assets/svg/heart_icon_filled.svg";
-                            //     onSavedPressed = () {
-                            //       GeoPoint sourceLocation =
-                            //           umapSourceDocuments[index]["location"];
-                            //       removeFromSavedList(
-                            //           UmapSaved(
-                            //             savedName: umapSourceDocuments[index]
-                            //                 ["name"],
-                            //             savedDescription:
-                            //                 umapSourceDocuments[index]
-                            //                     ["description"],
-                            //
-                            //             ///Todo: Replace Saved Distance with actual Saved distance
-                            //             savedDistance: "1.23km",
-                            //             savedTapLocationLatitude:
-                            //                 sourceLocation.latitude.toDouble(),
-                            //             savedTapLocationLongitude:
-                            //                 sourceLocation.longitude.toDouble(),
-                            //           ),
-                            //           umapSourceDocuments[index]["name"]);
-                            //     };
-                            //   } else {
-                            //     savedIconLink = "assets/svg/heart_icon.svg";
-                            //     onSavedPressed = () {
-                            //       GeoPoint sourceLocation =
-                            //           umapSourceDocuments[index]["location"];
-                            //       addToSavedList(
-                            //         UmapSaved(
-                            //           savedName: umapSourceDocuments[index]
-                            //               ["name"],
-                            //           savedDescription:
-                            //               umapSourceDocuments[index]
-                            //                   ["description"],
-                            //
-                            //           ///Todo: Replace Saved Distance with actual Saved distance
-                            //           savedDistance: "1.23km",
-                            //           savedTapLocationLatitude:
-                            //               sourceLocation.latitude.toDouble(),
-                            //           savedTapLocationLongitude:
-                            //               sourceLocation.longitude.toDouble(),
-                            //         ),
-                            //       );
-                            //     };
-                            //   }
-                            // }
                             return buildPopularPlacesList(
                               context,
                               document: umapSourceDocuments[index],
                             );
                           });
-                  }
-                },
+                    }
+                }
+              },
+            ),
+
+            ///Offices blocks Stream
+            SizedBox(
+              height: getRelativeScreenHeight(context, 10),
+            ),
+            Padding(
+              padding: EdgeInsets.all(getRelativeScreenWidth(context, 20)),
+              child: Text(
+                "Offices",
+                style: Theme.of(context).textTheme.headline2,
               ),
             ),
+            StreamBuilder(
+              stream: umapOfficesFirestoreStream,
+              builder: (
+                BuildContext context,
+                AsyncSnapshot<QuerySnapshot> snapshot,
+              ) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator.adaptive(
+                        backgroundColor: Colors.transparent,
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                          Theme.of(context).iconTheme.color!,
+                        ),
+                      ),
+                    );
+                  default:
+                    List<DocumentSnapshot> umapSourceDocuments =
+                        snapshot.data!.docs;
+                    if (umapSourceDocuments.length == 0) {
+                      return Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Text(
+                            "Oh, there doesn't seem to be anything under this category",
+                            textAlign: TextAlign.left,
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                        ),
+                      );
+                    } else {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemExtent: getRelativeScreenHeight(context, 195),
+                        itemCount: umapSourceDocuments.length,
+                        itemBuilder: (context, index) {
+                          return buildPopularPlacesList(
+                            context,
+                            document: umapSourceDocuments[index],
+                          );
+                        },
+                      );
+                    }
+                }
+              },
+            ),
+
+            ///Offices blocks Stream
+            SizedBox(
+              height: getRelativeScreenHeight(context, 10),
+            ),
+            Padding(
+              padding: EdgeInsets.all(getRelativeScreenWidth(context, 20)),
+              child: Text(
+                "Classes",
+                style: Theme.of(context).textTheme.headline2,
+              ),
+            ),
+            StreamBuilder(
+              stream: umapClassesFirestoreStream,
+              builder: (
+                BuildContext context,
+                AsyncSnapshot<QuerySnapshot> snapshot,
+              ) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator.adaptive(
+                        backgroundColor: Colors.transparent,
+                        valueColor: AlwaysStoppedAnimation<Color>(
+                          Theme.of(context).iconTheme.color!,
+                        ),
+                      ),
+                    );
+                  default:
+                    List<DocumentSnapshot> umapSourceDocuments =
+                        snapshot.data!.docs;
+                    if (umapSourceDocuments.length == 0) {
+                      return Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Text(
+                            "Oh, there doesn't seem to be anything under this category",
+                            textAlign: TextAlign.left,
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                        ),
+                      );
+                    } else {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemExtent: getRelativeScreenHeight(context, 195),
+                        itemCount: umapSourceDocuments.length,
+                        itemBuilder: (context, index) {
+                          return buildPopularPlacesList(
+                            context,
+                            document: umapSourceDocuments[index],
+                          );
+                        },
+                      );
+                    }
+                }
+              },
+            ),
+            SizedBox(
+              height: getRelativeScreenHeight(context, 100),
+            )
           ],
         ),
       ),
