d4c18507869da0dc63a9df6137abd1a8d1536190
nuilewis <lewisnuikweh@gmail.com>
2021-12-17 21:32:30 +0100

    Removed useless code, and made it Dark mode selector work using Provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:u_map/components/umap_shared_preferences/theme_model.dart';
 import 'package:u_map/screens/navigationscreen/umap_navscreen.dart';
 import 'package:u_map/theme.dart';
 import 'components/umap_bottom_nav_bar/umap_bottom_nav_bar.dart';
@@ -15,20 +17,20 @@ class UMap extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'U-Map',
-      theme: lightThemeData(context),
-      darkTheme: darkThemeData(context),
-      //themeMode: ThemeMode.light,
-      themeMode: ThemeMode.light,
-      debugShowCheckedModeBanner: false,
-      //home: UMapHomeScreen(),
-      // home: UMapFindScreen(),
-      home: UMapBottomNavBar(),
-      // home: UMapNavigationScreen(
-      //      name: 'null', description: 'null', directionInfo: null)
-      //home: UMapSavedScreen(),
-      //home: UmapLocationDetails(),
-    );
+    return ChangeNotifierProvider(
+        create: (BuildContext context) => ThemeModel(),
+        child: Consumer<ThemeModel>(
+            builder: (context, ThemeModel themeNotifier, child) {
+          return MaterialApp(
+            title: 'U-Map',
+            theme: lightThemeData(context),
+            darkTheme: darkThemeData(context),
+            //themeMode: ThemeMode.dark,
+            themeMode: themeNotifier.isDark ? ThemeMode.dark : ThemeMode.light,
+            debugShowCheckedModeBanner: false,
+
+            home: UMapBottomNavBar(),
+          );
+        }));
   }
 }
