51473f0f11c63bb4bd0595e9130c573ab8a2ce00
nuilewis <lewisnuikweh@gmail.com>
2022-01-10 03:39:06 +0100

    Removed unused imports, made saving robust and working

--- lib/screens/homescreen/components/umap_popular_places.dart
+++ lib/screens/homescreen/components/umap_popular_places.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -22,7 +21,7 @@ class _PopularPlacesState extends State<PopularPlaces> {
       .snapshots();
 
   late final LatLng markerLoc;
-  bool isSaved = false;
+  List<bool> isSaved = [];
 
   @override
   initState() {
@@ -35,15 +34,21 @@ class _PopularPlacesState extends State<PopularPlaces> {
       required DocumentSnapshot document,
       required int index,
       required String category}) {
+    for (int i = 0; i < umapSPList.length; i++) {
+      ///Check if is saved
+      if (umapSPList[i].savedID == document["id"][index]) {
+        isSaved[index] = true;
+      }
+    }
+
     ///Todo: Add code to find the most popular locations
     return PopularPlacesListItem(
       title: document["name"][index],
-      //markerGeopoint: document["location"][index],
       imageSrc: document["imageUrl"][index],
-      saveIconLink: isSaved
+      saveIconLink: isSaved[index]
           ? "assets/svg/heart_icon_filled.svg"
           : "assets/svg/heart_icon.svg",
-      onSavedPressed: isSaved
+      onSavedPressed: isSaved[index]
           ? () {
               Feedback.forTap(context);
               HapticFeedback.lightImpact();
@@ -57,7 +62,7 @@ class _PopularPlacesState extends State<PopularPlaces> {
                       savedImgUrl: document["imageUrl"][index]),
                   locationID: document["id"][index],
                 );
-                isSaved = false;
+                isSaved[index] = false;
               });
             }
           : () {
@@ -72,7 +77,7 @@ class _PopularPlacesState extends State<PopularPlaces> {
                       savedDescription: document["description"][index],
                       savedImgUrl: document["imageUrl"][index]),
                 );
-                isSaved = true;
+                isSaved[index] = true;
               });
             },
       onPressed: () {
@@ -97,101 +102,6 @@ class _PopularPlacesState extends State<PopularPlaces> {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
-    // ///Draggable Scrollable Sheet
-    // return DraggableScrollableSheet(
-    //   initialChildSize: .15,
-    //   maxChildSize: .55,
-    //   minChildSize: .15,
-    //   builder: (context, scrollController) {
-    //     return SingleChildScrollView(
-    //       controller: scrollController,
-    //       child: Container(
-    //         decoration: BoxDecoration(
-    //           // border: Border.all(color: Theme.of(context).primaryColor),
-    //           color: Theme.of(context).scaffoldBackgroundColor,
-    //           borderRadius: BorderRadius.vertical(
-    //               top: Radius.circular(getRelativeScreenWidth(context, 32)),
-    //               bottom: Radius.zero),
-    //         ),
-    //
-    //         //color: Colors.white,
-    //         width: screenWidth,
-    //         height: screenHeight * .465,
-    //         child: Stack(
-    //           children: [
-    //             Positioned(
-    //               top: getRelativeScreenHeight(context, 40),
-    //               left: getRelativeScreenHeight(context, 20),
-    //               child: Padding(
-    //                 padding: EdgeInsets.only(left: 8.0),
-    //                 child: Text(
-    //                   "Popular Places",
-    //                   style: Theme.of(context).textTheme.headline1,
-    //                   textAlign: TextAlign.left,
-    //                 ),
-    //               ),
-    //             ),
-    //             SizedBox(
-    //               height: getRelativeScreenHeight(context, 30),
-    //             ),
-    //             Padding(
-    //               padding: EdgeInsets.only(
-    //                 top: getRelativeScreenHeight(context, 120),
-    //                 //left: getRelativeScreenHeight(context, 20),
-    //               ),
-    //               child: StreamBuilder(
-    //                 stream: umapFirestoreStream,
-    //                 builder: (
-    //                   BuildContext context,
-    //                   AsyncSnapshot<QuerySnapshot> snapshot,
-    //                 ) {
-    //                   switch (snapshot.connectionState) {
-    //                     case ConnectionState.waiting:
-    //                       return Center(
-    //                         child: CircularProgressIndicator.adaptive(),
-    //                       );
-    //                     default:
-    //                       List<DocumentSnapshot> umapSourceDocuments =
-    //                           snapshot.data!.docs;
-    //                       return Container(
-    //                         height: getRelativeScreenHeight(context, 240),
-    //                         child: ListView.builder(
-    //                           scrollDirection: Axis.horizontal,
-    //                           //itemExtent: getRelativeScreenWidth(context, 240),
-    //                           itemCount: umapSourceDocuments.length,
-    //                           itemBuilder: (context, index) =>
-    //                               buildPopularPlacesList(
-    //                             context,
-    //                             umapSourceDocuments[index],
-    //                           ),
-    //                         ),
-    //                       );
-    //                   }
-    //                 },
-    //               ),
-    //             ),
-    //
-    //             ///Draggable icon indicator
-    //             Padding(
-    //               padding: const EdgeInsets.only(top: 10),
-    //               child: Align(
-    //                 alignment: Alignment.topCenter,
-    //                 child: Container(
-    //                   height: getRelativeScreenHeight(context, 5),
-    //                   width: getRelativeScreenWidth(context, 40),
-    //                   decoration: BoxDecoration(
-    //                     borderRadius: BorderRadius.all(Radius.circular(5)),
-    //                     color: Theme.of(context).accentColor,
-    //                   ),
-    //                 ),
-    //               ),
-    //             ),
-    //           ],
-    //         ),
-    //       ),
-    //     );
-    //   },
-    // );
     return Container(
       width: screenWidth,
       height: screenHeight * .465,
@@ -235,16 +145,20 @@ class _PopularPlacesState extends State<PopularPlaces> {
                     return Container(
                       height: getRelativeScreenHeight(context, 240),
                       child: ListView.builder(
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        //itemExtent: getRelativeScreenWidth(context, 240),
-                        itemCount: umapSourceDocuments["id"].length,
-                        itemBuilder: (context, index) => buildPopularPlacesList(
-                            context: context,
-                            document: umapSourceDocuments,
-                            index: index,
-                            category: "administrative blocks"),
-                      ),
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          //itemExtent: getRelativeScreenWidth(context, 240),
+                          itemCount: umapSourceDocuments["id"].length,
+                          itemBuilder: (context, index) {
+                            //Making all indexes of isSaved false
+                            isSaved.add(false);
+
+                            return buildPopularPlacesList(
+                                context: context,
+                                document: umapSourceDocuments,
+                                index: index,
+                                category: "administrative blocks");
+                          }),
                     );
                 }
               },
