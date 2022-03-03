289e5f88b1c4cc6bf970615427ea2a0f08fb9cd7
nuilewis <lewisnuikweh@gmail.com>
2022-03-03 04:05:43 +0100

    Changed direction API travel mode to walking

--- lib/components/umap_directions/directions_repository.dart
+++ lib/components/umap_directions/directions_repository.dart
@@ -12,15 +12,14 @@ class DirectionsRepository {
 
   DirectionsRepository({Dio? dio}) : _dio = dio ?? Dio();
 
-  Future<Directions> getDirections({
-    required LatLng origin,
-    required LatLng destination,
-  }) async {
+  Future<Directions> getDirections(
+      {required LatLng origin, required LatLng destination}) async {
     final response = await _dio.get(
       _baseUrl,
       queryParameters: {
         'origin': '${origin.latitude},${origin.longitude}',
         'destination': '${destination.latitude},${destination.longitude}',
+        'mode': 'walking',
         'key': googleAPIKey,
       },
     );
